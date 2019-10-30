58789827f01f8e4a3b72ea68594e394a1402bc34
martincavaliers <martincoutts5@gmail.com>
2019-10-30 13:29:48 +0000

    Timer input reading values into local state on change

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 
 export default class Timer extends Component {
+  state = {
+    // Sets default value to match element default value
+    time: 25
+  };
+
+  handleChange = event => {
+    this.setState({ time: event.target.value });
+  };
+
   render() {
     return (
       <div id="timer">
-        <input placeholder="25:00"></input>
+        <input
+          id="timeSelector"
+          type="number"
+          min="1"
+          max="60"
+          defaultValue="25"
+          onChange={this.handleChange}
+        ></input>
       </div>
     );
   }
